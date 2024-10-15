f080de9160ca67013a63642a5ecf152ff9420b1a
Bit-dagim <dagim.asrat@brothersitplc.com>
2024-10-15 16:30:47 +0300

    Added OTP page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./views/Home";
 import Login from "./features/Auth/Login";
 import Signup from "./features/Auth/Signup";
 import Sidebar from "./components/Sidebar";
 import UserManagement from "./features/UserManagement/UserManagement";
+import OTP from "./features/Auth/OTP";
 const App = () => {
   return (
     <Router>
@@ -13,6 +13,7 @@ const App = () => {
         <Route path="Signup" element={<Signup />} />
         <Route path="UserManagement" element={<UserManagement />} />
         <Route path="Sidebar" element={<Sidebar />} />
+        <Route path="OTP" element={<OTP />} />
       </Routes>
     </Router>
   );
