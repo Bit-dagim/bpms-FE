2eb25749c7a7248672735367e284f6dd72999c01
Bit-dagim <dagim.asrat@brothersitplc.com>
2024-10-15 17:07:53 +0300

    Minor change on signup page

--- src/features/Auth/Signup.jsx
+++ src/features/Auth/Signup.jsx
@@ -135,148 +135,27 @@ const Signup = () => {
                 />
               </div>
             </div>
-
-            {/* <div className="w-full">
+          </div>
+          <div className="mb-6 flex flex-col items-end gap-4 md:flex-row">
+            <div className="w-full">
               <Typography
                 variant="small"
                 color="blue-gray"
                 className="mb-2 font-medium"
               >
-                Birth Date
+                Email
               </Typography>
-              <Popover placement="bottom">
-                <PopoverHandler>
-                  <Input
-                    size="lg"
-                    onChange={() => null}
-                    placeholder="Select a Date"
-                    value={date ? format(date, "PPP") : ""}
-                    labelProps={
-                      {
-                        //   className: "hidden",
-                      }
-                    }
-                    className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
-                  />
-                </PopoverHandler>
-                <PopoverContent>
-                  <DayPicker
-                    mode="single"
-                    selected={date}
-                    onSelect={setDate}
-                    showOutsideDays
-                    className="border-0"
-                    classNames={{
-                      caption:
-                        "flex justify-center py-2 mb-4 relative items-center",
-                      caption_label: "text-sm !font-medium text-gray-900",
-                      nav: "flex items-center",
-                      nav_button:
-                        "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300",
-                      nav_button_previous: "absolute left-1.5",
-                      nav_button_next: "absolute right-1.5",
-                      table: "w-full border-collapse",
-                      head_row: "flex !font-medium text-gray-900",
-                      head_cell: "m-0.5 w-9 !font-normal text-sm",
-                      row: "flex w-full mt-2",
-                      cell: "text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-                      day: "h-9 w-9 p-0 !font-normal",
-                      day_range_end: "day-range-end",
-                      day_selected:
-                        "rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white",
-                      day_today: "rounded-md bg-gray-200 text-gray-900",
-                      day_outside:
-                        "day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10",
-                      day_disabled: "text-gray-500 opacity-50",
-                      day_hidden: "invisible",
-                    }}
-                    components={{
-                      IconLeft: ({ ...props }) => (
-                        <ChevronLeftIcon
-                          {...props}
-                          className="h-4 w-4 stroke-2"
-                        />
-                      ),
-                      IconRight: ({ ...props }) => (
-                        <ChevronRightIcon
-                          {...props}
-                          className="h-4 w-4 stroke-2"
-                        />
-                      ),
-                    }}
-                  />
-                </PopoverContent>
-              </Popover>
-            </div> */}
-            {/* <div className="w-full">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="mb-2 font-medium"
-              >
-                Day
-              </Typography>
-              <Select
+              <Input
                 size="lg"
+                placeholder="name.father'sname@brothersitplc.com"
                 labelProps={
                   {
                     //   className: "hidden",
                   }
                 }
-                className="border-t-blue-gray-200 aria-[expanded=true]:border-t-primary"
-              >
-                <Option>1</Option>
-                <Option>2</Option>
-                <Option>3</Option>
-                <Option>4</Option>
-                <Option>5</Option>
-                <Option>6</Option>
-                <Option>7</Option>
-                <Option>8</Option>
-                <Option>9</Option>
-                <Option>10</Option>
-                <Option>11</Option>
-                <Option>12</Option>
-                <Option>13</Option>
-                <Option>14</Option>
-                <Option>15</Option>
-                <Option>16</Option>
-                <Option>17</Option>
-                <Option>18</Option>
-                <Option>19</Option>
-                <Option>20</Option>
-                <Option>21</Option>
-                <Option>22</Option>
-                <Option>23</Option>
-                <Option>24</Option>
-                <Option>25</Option>
-                <Option>26</Option>
-                <Option>27</Option>
-                <Option>28</Option>
-                <Option>29</Option>
-                <Option>30</Option>
-              </Select>
-            </div> */}
-            {/* <div className="w-full">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="mb-2 font-medium"
-              >
-                Year
-              </Typography>
-              <Select
-                size="lg"
-                labelProps={{
-                  className: "hidden",
-                }}
-                className="border-t-blue-gray-200 aria-[expanded=true]:border-t-primary"
-              >
-                <Option>2022</Option>
-                <Option>2021</Option>
-                <Option>2020</Option>
-              </Select>
-            </div> */}
+                className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
+              />
+            </div>
           </div>
           <div className="mb-6 flex flex-col items-end gap-4 md:flex-row">
             <div className="w-full">
@@ -318,46 +197,7 @@ const Signup = () => {
               />
             </div>
           </div>
-          <div className="mb-6 flex flex-col items-end gap-4 md:flex-row">
-            <div className="w-full">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="mb-2 font-medium"
-              >
-                Email
-              </Typography>
-              <Input
-                size="lg"
-                placeholder="emma@mail.com"
-                labelProps={
-                  {
-                    //   className: "hidden",
-                  }
-                }
-                className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
-              />
-            </div>
-            {/* <div className="w-full">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="mb-2 font-medium"
-              >
-                Confirm Email
-              </Typography>
-              <Input
-                size="lg"
-                placeholder="emma@mail.com"
-                labelProps={
-                  {
-                    //   className: "hidden",
-                  }
-                }
-                className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
-              />
-            </div> */}
-          </div>
+
           <div className="mb-6 flex flex-col items-end gap-4 md:flex-row">
             <div className="w-full">
               <Typography
@@ -388,52 +228,17 @@ const Signup = () => {
               </a>
             </label>
           </div>
-
-          {/* <div className="flex flex-col items-end gap-4 md:flex-row">
-            <div className="w-full">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="mb-2 font-medium"
-              >
-                Language
-              </Typography>
-              <Input
-                size="lg"
-                placeholder="Language"
-                labelProps={{
-                  className: "hidden",
-                }}
-                className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
-              />
-            </div>
-            <div className="w-full">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="mb-2 font-medium"
-              >
-                Skills
-              </Typography>
-              <Input
-                size="lg"
-                placeholder="Skills"
-                labelProps={{
-                  className: "hidden",
-                }}
-                className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
-              />
-            </div>
-          </div> */}
         </div>{" "}
-        <DialogFooter>
-          <Button variant="text" color="black" className="mr-1">
-            <span>Cancel</span>
-          </Button>
-          <Button variant="gradient" color="purple">
-            <span>{"Signup"}</span>
-          </Button>
-        </DialogFooter>
+        <div className="flex justify-start">
+          <DialogFooter>
+            {/* <Button variant="text" color="black" className="mr-1">
+              <span>Cancel</span>
+            </Button> */}
+            <Button size="lg" variant="gradient" color="purple">
+              <span>{"Signup"}</span>
+            </Button>
+          </DialogFooter>
+        </div>
       </section>
     </div>
   );
